a93e382ff184aa65d7464b5fb9ee6bb87a183749
kjy5657 <45618920+kjy5657@users.noreply.github.com>
2022-11-16 23:37:01 +0900

    programmers

---
+++ javascript/note/permutation,combination.js
@@ -0,0 +1,34 @@
+const getPermutations = (array, selectNumber) => {
+  const results = [];
+  if (selectNumber === 1) {
+    return array.map((element) => [element]);
+  }
+  array.forEach((fixed, index, origin) => {
+    const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
+    const permutations = getPermutations(rest, selectNumber - 1);
+    const attached = permutations.map((permutation) => [fixed, ...permutation]);
+
+    results.push(...attached);
+  });
+  return results;
+};
+
+//3P3=3!
+console.log(getPermutations([1, 2, 3], 3));
+
+const getCombinations = (array, selectNumber) => {
+  const results = [];
+  if (selectNumber === 1) {
+    return array.map((element) => [element]);
+  }
+  array.forEach((fixed, index, origin) => {
+    const rest = origin.slice(index + 1);
+    const combinations = getCombinations(rest, selectNumber - 1);
+    const attached = combinations.map((combination) => [fixed, ...combination]);
+    results.push(...attached);
+  });
+  return results;
+};
+
+//4C3
+console.log(getCombinations([1, 2, 3, 4], 3));
