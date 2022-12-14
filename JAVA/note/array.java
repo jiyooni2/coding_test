789fb7fa6b3e9f3f27293d288aaa015ed3952e8d
kjy5657 <45618920+kjy5657@users.noreply.github.com>
2022-12-14 16:26:37 +0900

    Java - Greedy

---
+++ JAVA/note/array.java
@@ -0,0 +1,16 @@
+package JAVA.note;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class array {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4 };
+
+        // sort
+        Arrays.sort(arr);
+        System.out.println(arr);
+    }
+
+}
